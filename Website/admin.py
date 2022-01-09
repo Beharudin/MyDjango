9d3ebbb0a7cac4a8d4fd48bd8b545a7252d6680b
Beharudin <89333678+Beharudin@users.noreply.github.com>
2022-01-09 15:34:19 +0300

    Update admin.py

--- Website/admin.py
+++ Website/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Game, Player, Table
+from .models import Game, Player, Table, Champ
 
 # Register your models here.
 admin.site.register(Game),
 admin.site.register(Player),
-admin.site.register(Table)
+admin.site.register(Table),
+admin.site.register(Champ)
